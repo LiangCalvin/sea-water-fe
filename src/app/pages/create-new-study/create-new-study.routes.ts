bd04c4ca901bbc8ddba3631f86ec3444dcdad31c
Calvin Liang <145249380+LiangCalvin@users.noreply.github.com>
2025-10-18 15:25:25 +0700

    add pusher

--- src/app/pages/create-new-study/create-new-study.routes.ts
+++ src/app/pages/create-new-study/create-new-study.routes.ts
@@ -1,12 +1,13 @@
 import { Routes } from '@angular/router';
 import { CreateNewStudyComponent } from './create-new-study.component';
 import { unsavedChangesGuard } from '../../guards/unsaved-changes.guard';
+import { ChatComponent } from '../calculation/components/recommendation/component/chat/chat.component';
 
 export const CreateNewStudyRoutes: Routes = [
   {
     path: 'create-new-study',
     children: [],
-    component: CreateNewStudyComponent,
+    component: ChatComponent,
     canDeactivate: [unsavedChangesGuard]
   },
 ];
